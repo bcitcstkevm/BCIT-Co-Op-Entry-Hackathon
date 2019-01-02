487b58e0d976bb7e1542540449e8483e05ae21bf
Edward Lee <edwardlee@edward.wifi.bcit.ca>
2019-01-02 14:26:13 -0800

    audio slow playback and json parse start

---
+++ src/js/mainscript.js
@@ -0,0 +1,122 @@
+/** randomizeBlocks Jquery Plugin.
+* Simple jquery plugin to randomize block movements
+* Divs must be first fixed to work.
+* Call on an array of jquery object.
+*/
+(function($) {
+
+  $.fn.randomizeBlocks = function() {
+    return this.each(function() {
+            animateDiv($(this));
+    });
+  };
+
+  function makeNewPosition($container) {
+      // Get viewport dimensions (remove the dimension of the div)
+      var h = $container.height() - 10;
+      var w = $container.width() - 10;
+
+      var nh = Math.floor(Math.random() * h);
+      var nw = Math.floor(Math.random() * w);
+
+      return [nh, nw];
+
+  }
+
+  function animateDiv($target) {
+      var newq = makeNewPosition($target.parent());
+      var oldq = $target.offset();
+      var speed = calcSpeed([oldq.top, oldq.left], newq);
+
+      $target.animate({
+          top: newq[0],
+          left: newq[1]
+      }, speed, function() {
+          animateDiv($target);
+      });
+
+  };
+
+  function calcSpeed(prev, next) {
+
+      var x = Math.abs(prev[1] - next[1]);
+      var y = Math.abs(prev[0] - next[0]);
+
+      var greatest = x > y ? x : y;
+
+      var speedModifier = 0.1;
+
+      var speed = Math.ceil(greatest / speedModifier);
+
+      return speed;
+
+  }
+
+}( jQuery ));
+
+//----------------------------------------------
+// DUMMY DATA______________
+
+
+var dummy = "vocabulary";
+
+
+
+//----------------------------------------------
+
+// Function to get the Ultimate Json for audio links
+function getUltimate(){
+  return $.getJSON('./src/data/ultimate.json');
+}
+
+$.getJSON('./src/data/ultimate.json', function(json) {
+  console.log(json.);
+})
+
+
+
+
+$('#test').on('click', function() {
+  $(this).randomizeBlocks();
+})
+
+
+
+// DOCUMENT READY
+$(document).ready(function() {
+
+
+  var audio = document.getElementById('word-audio');
+
+  $('.syllable').randomizeBlocks();
+
+
+  $('#half-speed').on('click', function() {
+    audio.playbackRate = 0.5;
+  });
+
+  $('#quarter-speed').on('click', function() {
+    audio.playbackRate = 0.75;
+  });
+
+  $('#full-speed').on('click', function() {
+    audio.playbackRate = 1.0;
+  });
+
+  $('#two-speed').on('click', function() {
+    audio.playbackRate = 2.0;
+  });
+
+  // getUltimate().done(function(json) {
+  //   console.log(Object.keys(json).length);
+  //   console.log(json);
+  // })
+
+
+})
+
+function parseaws(word, json) {
+  for (var i = 0; i < json.length; i++) {
+
+  }
+}
